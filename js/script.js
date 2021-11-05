3d90cb88880a0ed81bfefd3d01ca2b0090aa6331
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-11-05 17:49:24 +0100

    Aggiornamento esercizio

--- js/script.js
+++ js/script.js
@@ -52,88 +52,68 @@ const post = [
     }
 ]
 
+innerFunction ();
+        
+    let miPiace = document.querySelectorAll(".likes__cta");
+    // console.log(miPiace);
+    for ( let i = 0 ; i < miPiace.length; i++){
+        let iLike = miPiace[i];
+        // console.log(iLike);
+        iLike.addEventListener("click",function(){
+            console.log("ciao");
+            const index = this.getAttribute("data-postid");
+            post[index].likes = post[index].likes +1
 
-// function miaFunzione(){
+        
+    })
+}
+
+    
+
+
+function innerFunction (){
     for ( let i = 0 ; i < post.length; i++){
         let OggettoPost = post[i]
         let {nome,fotoProfilo,data,testoPost,immagine,likes} = OggettoPost;
-        let innerPostHtml = `
-        <div class="post">
-                <div class="post__header">
-                    <div class="post-meta">                    
-                        <div class="post-meta__icon">
-                            <img class="profile-pic" src="${fotoProfilo}" alt="${nome}">                    
-                        </div>
-                        <div class="post-meta__data">
-                            <div class="post-meta__author">${nome}</div>
-                            <div class="post-meta__time">${data}</div>
-                        </div>                    
+    let innerPostHtml = `
+    <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${fotoProfilo}" alt="${nome}">                    
                     </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${nome}</div>
+                        <div class="post-meta__time">${data}</div>
+                    </div>                    
                 </div>
-                <div class="post__text">${testoPost} </div>
-                <div class="post__image">
-                    <img src="${immagine}" alt="">
-                </div>
-                <div class="post__footer">
-                    <div class="likes js-likes">
-                        <div class="likes__cta">
-                            <a class="like-button  js-like-button" href="#" data-postid="1">
-                                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                                <span class="like-button__label">Mi Piace</span>
-                            </a>
-                        </div>
-                        <div class="likes__counter">
-                            Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
-                        </div>
-                    </div> 
-                </div>            
             </div>
-        `
-    
-
-        // for ( let key in OggettoPost) {
-        //     if ( key = undefined){
-        //         innerPostHtml =`
-        //         <div class="post">
-        //         <div class="post__header">
-        //             <div class="post-meta">                    
-        //                 <div class="post-meta__icon">
-        //                     <img class="profile-pic" src="${fotoProfilo}" alt="${nome}">                    
-        //                 </div>
-        //                 <div class="post-meta__data">
-        //                     <div class="post-meta__author">${nome}</div>
-        //                     <div class="post-meta__time">${data}</div>
-        //                 </div>                    
-        //             </div>
-        //         </div>
-        //         <div class="post__text">${testoPost} </div>
-        //         <div class="post__image">
-        //             <img src="${immagine}" alt="">
-        //         </div>
-        //         <div class="post__footer">
-        //             <div class="likes js-likes">
-        //                 <div class="likes__cta">
-        //                     <a class="like-button  js-like-button" href="#" data-postid="1">
-        //                         <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-        //                         <span class="like-button__label">Mi Piace</span>
-        //                     </a>
-        //                 </div>
-        //                 <div class="likes__counter">
-        //                     Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
-        //                 </div>
-        //             </div> 
-        //         </div>            
-        //     </div>
-        //         `
-        //     }
-        // }
-        
-        
-        containerPost.innerHTML += innerPostHtml;
-        const miPiace = document.querySelector(".likes__cta")
-        miPiace.addEventListener("click",function(){
-            console.log("iterazione n:",i, miPiace);
-        })
-
+            <div class="post__text">${testoPost} </div>
+            
+            <div class="post__image">
+                <img src="${immagine}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="${i}">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+`
+containerPost.innerHTML += innerPostHtml;
 
 }
+
+}
+
+// if (!immagine) {
+            
+        // }
