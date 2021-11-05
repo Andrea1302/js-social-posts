805cf2055b74e0a1564a8e7ec9952eda66dc39f4
Andrea Scarpa <scarpandrea.1996@hotmail.com>
2021-11-05 16:07:27 +0100

    Prova dei mi piace

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ numero di likes.
 //  se clicco sul pulsante mi piace, incrementerò il numero dei likes ----> condizione click
 
 // var globale
-const containerPosto = document.getElementById("container");
+const containerPost = document.getElementById("container");
 
 
 // Array di oggetti 
@@ -52,41 +52,88 @@ const post = [
     }
 ]
 
-for ( let i = 0 ; i < post.length; i++){
-    let OggettoPost = post[i]
-    console.log(OggettoPost);
-    const {nome,fotoProfilo,data,testoPost,immagine,likes} = OggettoPost;
-    let innerPostHtml = `
-    <div class="post">
-            <div class="post__header">
-                <div class="post-meta">                    
-                    <div class="post-meta__icon">
-                        <img class="profile-pic" src="${fotoProfilo}" alt="${nome}">                    
+
+// function miaFunzione(){
+    for ( let i = 0 ; i < post.length; i++){
+        let OggettoPost = post[i]
+        let {nome,fotoProfilo,data,testoPost,immagine,likes} = OggettoPost;
+        let innerPostHtml = `
+        <div class="post">
+                <div class="post__header">
+                    <div class="post-meta">                    
+                        <div class="post-meta__icon">
+                            <img class="profile-pic" src="${fotoProfilo}" alt="${nome}">                    
+                        </div>
+                        <div class="post-meta__data">
+                            <div class="post-meta__author">${nome}</div>
+                            <div class="post-meta__time">${data}</div>
+                        </div>                    
                     </div>
-                    <div class="post-meta__data">
-                        <div class="post-meta__author">${nome}</div>
-                        <div class="post-meta__time">${data}</div>
-                    </div>                    
                 </div>
+                <div class="post__text">${testoPost} </div>
+                <div class="post__image">
+                    <img src="${immagine}" alt="">
+                </div>
+                <div class="post__footer">
+                    <div class="likes js-likes">
+                        <div class="likes__cta">
+                            <a class="like-button  js-like-button" href="#" data-postid="1">
+                                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                <span class="like-button__label">Mi Piace</span>
+                            </a>
+                        </div>
+                        <div class="likes__counter">
+                            Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+                        </div>
+                    </div> 
+                </div>            
             </div>
-            <div class="post__text">${testoPost} </div>
-            <div class="post__image">
-                <img src="${immagine}" alt="">
-            </div>
-            <div class="post__footer">
-                <div class="likes js-likes">
-                    <div class="likes__cta">
-                        <a class="like-button  js-like-button" href="#" data-postid="1">
-                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                            <span class="like-button__label">Mi Piace</span>
-                        </a>
-                    </div>
-                    <div class="likes__counter">
-                        Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
-                    </div>
-                </div> 
-            </div>            
-        </div>
-    `
-    containerPosto.innerHTML += innerPostHtml
+        `
+    
+
+        // for ( let key in OggettoPost) {
+        //     if ( key = undefined){
+        //         innerPostHtml =`
+        //         <div class="post">
+        //         <div class="post__header">
+        //             <div class="post-meta">                    
+        //                 <div class="post-meta__icon">
+        //                     <img class="profile-pic" src="${fotoProfilo}" alt="${nome}">                    
+        //                 </div>
+        //                 <div class="post-meta__data">
+        //                     <div class="post-meta__author">${nome}</div>
+        //                     <div class="post-meta__time">${data}</div>
+        //                 </div>                    
+        //             </div>
+        //         </div>
+        //         <div class="post__text">${testoPost} </div>
+        //         <div class="post__image">
+        //             <img src="${immagine}" alt="">
+        //         </div>
+        //         <div class="post__footer">
+        //             <div class="likes js-likes">
+        //                 <div class="likes__cta">
+        //                     <a class="like-button  js-like-button" href="#" data-postid="1">
+        //                         <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+        //                         <span class="like-button__label">Mi Piace</span>
+        //                     </a>
+        //                 </div>
+        //                 <div class="likes__counter">
+        //                     Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+        //                 </div>
+        //             </div> 
+        //         </div>            
+        //     </div>
+        //         `
+        //     }
+        // }
+        
+        
+        containerPost.innerHTML += innerPostHtml;
+        const miPiace = document.querySelector(".likes__cta")
+        miPiace.addEventListener("click",function(){
+            console.log("iterazione n:",i, miPiace);
+        })
+
+
 }
